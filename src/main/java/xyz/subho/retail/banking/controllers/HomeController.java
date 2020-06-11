7731aad38980bfec73c3684e0f6d8a03a7d03bcf
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-06-11 06:53:23 +0530

    created hello world

---
+++ src/main/java/xyz/subho/retail/banking/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package xyz.subho.retail.banking.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home()	{
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+	public String index()	{
+		return "index";
+	}
+
+}
