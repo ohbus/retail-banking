e6234f2a96cbcf2aa79515c0fa6419877411e505
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-06-12 16:06:20 +0530

    WIP: service module completed

---
+++ src/main/java/xyz/subho/retail/banking/security/Authority.java
@@ -0,0 +1,17 @@
+package xyz.subho.retail.banking.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+
+	private final String authority;
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
