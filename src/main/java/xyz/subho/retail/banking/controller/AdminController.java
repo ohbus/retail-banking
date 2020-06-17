23b36839af3fe50181dcf15fdc5d925fcc031ac8
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-06-17 10:09:55 +0530

    Admin Signup created

--- src/main/java/xyz/subho/retail/banking/controller/AdminController.java
+++ src/main/java/xyz/subho/retail/banking/controller/AdminController.java
@@ -1,7 +1,9 @@
 package xyz.subho.retail.banking.controller;
 
 import java.security.Principal;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import xyz.subho.retail.banking.dao.RoleDao;
 import xyz.subho.retail.banking.model.CurrentAccount;
 import xyz.subho.retail.banking.model.CurrentTransaction;
 import xyz.subho.retail.banking.model.SavingsAccount;
 import xyz.subho.retail.banking.model.SavingsTransaction;
 import xyz.subho.retail.banking.model.User;
+import xyz.subho.retail.banking.security.UserRole;
 import xyz.subho.retail.banking.service.TransactionService;
 import xyz.subho.retail.banking.service.UserService;
 
@@ -28,6 +32,9 @@ public class AdminController {
 
 	@Autowired
 	private TransactionService transactionService;
+	
+    @Autowired
+    private RoleDao roleDao;
 
 	@GetMapping("/panel")
 	public String profile(Principal principal, Model model) {
@@ -93,5 +100,63 @@ public class AdminController {
 		return "savingsAccount";
 
 	}
+	
+    @GetMapping("/signup")
+    public String signup(Model model, Principal principal) {
+    	
+		if (!principal.getName().equals("Admin"))
+			return "error";
+    	
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "adminSignup";
+        
+    }
+	
+    @SuppressWarnings("finally")
+	@PostMapping("/signup")
+    public String signupPost(@ModelAttribute("user") User user, Model model, Principal principal) {
+    	
+		if (!principal.getName().equals("Admin"))
+			return "error";
+
+        if (userService.checkUserExists(user.getUsername(), user.getEmail())) {
+
+            if (userService.checkEmailExists(user.getEmail())) {
+            	
+                model.addAttribute("emailExists", true);
+                
+            }
+
+            if (userService.checkUsernameExists(user.getUsername())) {
+            	
+                model.addAttribute("usernameExists", true);
+                
+            }
+
+            return "adminSignup";
+            
+        } else {
+        	try	{
+        	
+	            Set<UserRole> userRoles = new HashSet<>();
+	            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_ADMIN")));
+	
+	            userService.createUser(user, userRoles);
+        	}
+        	catch(Exception e)	{
+        		model.addAttribute("errorSignUp", true);
+        		e.printStackTrace();
+        	}
+        	finally	{
+        		model.addAttribute("successSignUp", true);
+        		return "redirect:/";
+        	}
+            
+        }
+        
+    }
 
 }
