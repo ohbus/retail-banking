8678db9b321b333ad22ac969d7fd843468fc3dad
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-06-17 06:25:53 +0530

    admin controller view completed

--- src/main/java/xyz/subho/retail/banking/controller/AdminController.java
+++ src/main/java/xyz/subho/retail/banking/controller/AdminController.java
@@ -10,8 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import xyz.subho.retail.banking.model.CurrentAccount;
+import xyz.subho.retail.banking.model.CurrentTransaction;
+import xyz.subho.retail.banking.model.SavingsAccount;
+import xyz.subho.retail.banking.model.SavingsTransaction;
 import xyz.subho.retail.banking.model.User;
+import xyz.subho.retail.banking.service.AccountService;
+import xyz.subho.retail.banking.service.TransactionService;
 import xyz.subho.retail.banking.service.UserService;
 
 @Controller
@@ -20,9 +27,17 @@ public class AdminController {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private TransactionService transactionService;
 
     @GetMapping("/panel")
     public String profile(Principal principal, Model model) {
+    	
+    	String str = principal.getName();
+    	
+    	if(!principal.getName().equals("Admin"))
+    		return "error403";
     	
         List<User> userList = userService.findUserList();
 
@@ -33,7 +48,10 @@ public class AdminController {
     }
 
     @PostMapping("/deleteUser")
-    public String profilePost(@ModelAttribute("user") User newUser, Model model) {
+    public String profilePost(@ModelAttribute("user") User newUser, Model model, Principal principal) {
+    	
+    	if(principal.getName()!="Admin")
+    		return "error403";
     	
         User user = userService.findByUsername(newUser.getUsername());
         user.setUsername(newUser.getUsername());
@@ -48,6 +66,41 @@ public class AdminController {
         userService.saveUser(user);
 
         return "profile";
+        
+    }
+    
+    @RequestMapping("/currentAccount")
+    public String currentAccount(Model model, Principal principal) {
+    	
+    	if(principal.getName()!="Admin")
+    		return "error403";
+       
+    	List<CurrentTransaction> currentTransactionList = transactionService.findCurrentTransactionList(principal.getName());
+
+        User user = userService.findByUsername(principal.getName());
+        CurrentAccount currentAccount = user.getCurrentAccount();
+
+        model.addAttribute("currentAccount", currentAccount);
+        model.addAttribute("currentTransactionList", currentTransactionList);
+
+        return "currentAccount";
+        
+    }
+
+    @RequestMapping("/savingsAccount")
+    public String savingsAccount(Model model, Principal principal) {
+    	
+    	if(principal.getName()!="Admin")
+    		return "error403";
+        
+    	List<SavingsTransaction> savingsTransactionList = transactionService.findSavingsTransactionList(principal.getName());
+        User user = userService.findByUsername(principal.getName());
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("savingsAccount", savingsAccount);
+        model.addAttribute("savingsTransactionList", savingsTransactionList);
+
+        return "savingsAccount";
         
     }
 
